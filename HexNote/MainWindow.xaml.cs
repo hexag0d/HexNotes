864d881b2d6a222ddc1a60fb2457329f0ece01a5
hexagod <36330237+hexag0d@users.noreply.github.com>
2019-08-03 11:54:18 -0700

    created repo

---
+++ HexNote/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HexNote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            notesTextBox.AcceptsReturn = true;
+        }
+
+        private void TopCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (topCheckBox.IsChecked == true)
+            {
+                this.Topmost = true;
+            }
+            else
+            {
+                this.Topmost = false;
+            }
+        }
+
+        private void OpacitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.Opacity = opacitySlider.Opacity;
+        }
+    }
+}
